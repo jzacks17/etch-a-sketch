60ab4ebbc6b08ce830358f0e58d596176049a4d8
Josh <16jmbz@queensu.ca>
2023-03-15 16:28:42 -0400

    Added Changing Grid Feature
    Added javascript code, made changes to index.html and styles.css to
    implement the changing grid feature.
    
    Grid size changes with the use of a slider and the current grid size is
    displayed above the slider.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+//------------------------------------------ Variable Declaration Section ----------------------------------------------
+
 // create a variable of type const called container equal to the container div (document.querySelector)
 const container = document.querySelector('.container');
 
 // create a variable of type const called size. This variable will store the dimension of the etch a sketch grid
-const size = 16;
+let size = 32;
 
 // create a variable of type let called row. Do not assign an initial value. This variable will be used to create the row divs. 
 let row;
@@ -10,40 +12,72 @@ let row;
 // create a variable of type let called pixel. Do not assign an initial value. This variable will be used to create the pixel divs.
 let pixel;
 
-//For loop to create each row of the grid (loop from 1 to size)
-for (let i = 1; i <= size; i++) {
+// create a variable of type let called pixels which is equal to all the pixel divs. Do not assign an initial value. 
+let pixels;
 
-    //set the value of row to equal a new div element 
-    row = document.createElement('div');
-    //add the class 'row' to the row div element
-    row.classList.add('row');
-    //append the row div element to the container div
-    container.appendChild(row);
+// create a variable of type const called slider. Assign an initial value of the slider element 
+const slider = document.querySelector('.slider');
 
-    //for loop to create each pixel in the row (loop from 1 to size)
-    for (let j = 1; j <= size; j++) {
 
-        //set the value of pixel to equal a new div element
-        pixel = document.createElement('div');
-        //add the class 'pixel' to the row div element
-        pixel.classList.add('pixel');
-        //append the pixel div element to the current row div
-        row.appendChild(pixel);
-    }
+// ----------------------------------------------- Create Grid section------------------------------------------------------
+
+//create the defualt grid
+createGrid(size);
+
+
+//On input for slider:
+slider.oninput = function () {
+
+    size = slider.value;
+
+    //set the dimensions inner text to be the size
+    document.querySelector('.dimension').innerText = `${size} × ${size}`;
+
+    //call createGrid function to re-size the grid
+    createGrid(size);
 }
 
-// create a variable of type const called pixels which is equal to all the pixel divs (querySelectorAll)
-const pixels = document.querySelectorAll('.pixel');
+//-------------------------------------------------Function Section ------------------------------------------------
 
-// add eventlistener for each pixel of pixels. On hover, change the background color to black 
-pixels.forEach(pixel => pixel.addEventListener('mouseover', changeColour));
-
-    
-function changeColour(){
+//function that changes the colour of a pixel when it is hovered over
+function changeColour() {
     this.style.backgroundColor = 'black';
 }
 
+function createGrid(n) {
 
+    //remove all children of container
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 
+    //For loop to create each row of the grid (loop from 1 to size)
+    for (let i = 1; i <= n; i++) {
+
+        //set the value of row to equal a new div element 
+        row = document.createElement('div');
+        //add the class 'row' to the row div element
+        row.classList.add('row');
+        //append the row div element to the container div
+        container.appendChild(row);
+
+        //for loop to create each pixel in the row (loop from 1 to size)
+        for (let j = 1; j <= n; j++) {
+
+            //set the value of pixel to equal a new div element
+            pixel = document.createElement('div');
+            //add the class 'pixel' to the row div element
+            pixel.classList.add('pixel');
+            //append the pixel div element to the current row div
+            row.appendChild(pixel);
+        }
+    }
+
+    //set value of pixels to all the pixels
+    pixels = document.querySelectorAll('.pixel');
+    
+    // add eventlistener for each pixel of pixels. On hover, run the changeColour function
+    pixels.forEach(pixel => pixel.addEventListener('mouseover', changeColour));
+}
 
 
